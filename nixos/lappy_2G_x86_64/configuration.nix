82856ecd414041b3842545bc0c8e8ff88f61ac18
david-bfg <david.1.bfg@gmail.com>
2026-08-11 16:08:46 -0500

    Add postgresql database to home assistant service

--- nixos/lappy_2G_x86_64/configuration.nix
+++ nixos/lappy_2G_x86_64/configuration.nix
@@ -105,6 +105,25 @@
   services.home-assistant = {
     enable = true;
     configDir = "/home/dbg/services/home-assistant";
+    package = (pkgs.home-assistant.override {
+      extraPackages = py: with py; [ psycopg2 ];
+    }).overrideAttrs (oldAttrs: {
+      doInstallCheck = false;
+    });
+    config.recorder.db_url = "postgresql://@/hass";
+  };
+
+  ############################################################
+  # PostgreSQL
+  ############################################################
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "hass" ];
+    ensureUsers = [{
+      name = "hass";
+      ensureDBOwnership = true;
+    }];
   };
 
   ############################################################
